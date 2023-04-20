78e499b7d6d83488fb6ab7a9d91787848205e942
Aram Karapetyan <aram@dasmeta.com>
2023-04-20 18:40:14 +0400

    feat(fix): Fix version

--- modules/auth0-auth-google/main.tf
+++ modules/auth0-auth-google/main.tf
@@ -2,7 +2,7 @@ resource "auth0_connection" "google_oauth2" {
   name                 = "google-oauth2"
   strategy             = "google-oauth2"
   is_domain_connection = var.is_domain_connection
-  enabled_clients      = var.enabled_clients
+  # enabled_clients      = var.enabled_clients
   realms = [
     "google-oauth2",
   ]
